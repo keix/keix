982e4026a69ec1aaa21b0934647b6cbffcc4c6fd
kei sawamura <kei@a-wtk.net>
2015-11-13 05:24:24 +0900

    Update keix.lisp

--- keix.lisp
+++ keix.lisp
@@ -1,12 +1,1 @@
 (defun keix (hofchan) (cons hofchan nil))
-
-(defun soro-soro (x)
-    (dotimes (i x)
-        (if (= i x)
-            (keix (hofchan i))
-            (keix (hofchan i)))))
-
-(defun hofchan (x) (print x))
-
-(keix (soro-soro 2))
-
